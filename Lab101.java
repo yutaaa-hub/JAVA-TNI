e4e569e0fc0d0be752d84a6d6c1ca269e9e936d5
yutaaa-hub <yuta6066y@gmail.com>
2025-11-18 13:56:26 +0700

    Yuta Voraborvorn Labsheet1

---
+++ Lab101.java
@@ -0,0 +1,8 @@
+public class Lab101 {
+    public static void main(String[] args) {
+        System.out.println("Yuta Voraborvorn");
+        System.out.println("\"INT-108\" Object-Oriented Programming");
+        System.out.println("\tThis is my first time learning Java");
+        System.out.println("\tI hope to get grade \"A\"");
+    }
+}
---
+++ Lab105.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class Lab105 {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,
+                "Summation number = " + (2 + 5)
+                +"\nSubtraction number = " + (2 - 5)
+                +"\nMultiplication number = " + (2 * 5)
+                +"\nDivision number = " + (2 / 5f)
+                +"\nModulus number = " + (2 % 5),
+                "Arithmetic Operators", JOptionPane.WARNING_MESSAGE);
+
+
+
+    }
+}
